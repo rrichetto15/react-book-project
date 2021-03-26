fcf69e6c6e556b295b1dfdd6aa15ff43208fa786
Ryan Richetto <rrichetto15@gmail.com>
2021-03-25 22:30:06 -0700

    complete project

---
+++ src/Book.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Book = ({ img, title, author }) => {
+  const clickHandler = e => {
+    alert('Hello World');
+  };
+
+  const complexExample = author => {
+    console.log(author);
+  };
+
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Reference Example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More Complex Example
+      </button>
+    </article>
+  );
+};
+
+export default Book;
